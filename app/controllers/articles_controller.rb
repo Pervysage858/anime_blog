8e9e9fd3c38c377bec3395b799643a5237202fd1
Prathamesh Desai <prathamesh.desai@theadroir.in>
2024-10-14 11:36:39 +0530

    Add users create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,46 +1,46 @@
 class ArticlesController < ApplicationController
-	
-	before_action :set_article, only: [:show, :edit, :destroy]
+  
+  before_action :set_article, only: [:show, :edit, :destroy]
 
-	def show 
+  def show 
   end
 
   def index
-  	@articles = Article.all
+    @articles = Article.all
   end
 
   def new
-  	@article = Article.new
+    @article = Article.new
   end
 
   def create
-  	@article=Article.new(article_params)
-  	if @article.save
-  		redirect_to (@article)
-
-  	else
-  		render 'new'
-
-  	end
-  	
+    @article = Article.new(article_params)
+    @article.user = User.first
+    if @article.save
+      flash[:notice]
+      redirect_to (@article)
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
   def update 
-  	@article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
-    	flash[:notice]="Article was succesfully updated"
-    	redirect_to @article
+    byebug
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description, :user_id))
+      flash[:notice]="Article was succesfully updated"
+      redirect_to @article
     else
-    	render 'edit'
+      render 'edit'
     end
   end
 
   def destroy 
-  	@article.destroy
-  	redirect_to articles_path
+    @article.destroy
+    redirect_to articles_path
   end
 
 
@@ -48,11 +48,11 @@ class ArticlesController < ApplicationController
   private 
 
   def set_article
-  	@article = Article.find(params[:id])
+    @article = Article.find(params[:id])
   end
 
   def article_params
-  	params.require(:article).permit(:title, :description)
+    params.require(:article).permit(:title, :description, :user_id)
   end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_10_11_052244) do
+ActiveRecord::Schema.define(version: 2024_10_11_073142) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -24,6 +25,7 @@ ActiveRecord::Schema.define(version: 2024_10_11_052244) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
